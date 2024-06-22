fc76939dcf439e2eb9cef4984ad5408c0fdb5e35
Dyson <dysonkui@gmail.com>
2024-06-22 19:39:42 +0400

    Added bomb behavior (need effects)

---
+++ Assets/Dyson/Scripts/BombBehavior.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombBehavior : MonoBehaviour
+{
+    public GameObject bomb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bomb = GameObject.Find("Bomb");
+        Invoke("ChangeBombColor", 3);
+    }
+
+    void ChangeBombColor()
+    {
+        Debug.Log("Im here");
+        var bombRenderer = bomb.GetComponent<Renderer>();
+        bombRenderer.material.SetColor("_BaseColor", Color.red);
+        Debug.Log("Explosion");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
