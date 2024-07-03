ed1383580d0a36c56c6285c7533f2273142a40fb
Leonardo Estigarribia <leonardo.estigarribia@student.saedubai.com>
2024-07-03 13:54:25 +0400

    First commit of Radio Puzzle [In proccess]

---
+++ Assets/Scripts/RythmRadioPuzzle/RythmRadioPuzzle.cs
@@ -0,0 +1,52 @@
+using System;
+using Kandooz.InteractionSystem.Core;
+using Kandooz.InteractionSystem.Interactions;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Leonardo.RythmRadioPuzzle
+{
+    public class RythmRadioPuzzle : MonoBehaviour
+    {
+        [SerializeField] private bool blueButtonPressed = false;
+        [SerializeField] private bool yellowButtonPressed = false;
+        [SerializeField] private bool greenButtonPressed = false;
+        [SerializeField] private bool redButtonPressed = false;
+
+        private int buttonsTimesPressed = 0; // Counter of the times the buttons were pressed.
+
+        private void Update()
+        {
+            // Check 
+        }
+
+        public void BlueButtonPressed()
+        {
+            buttonsTimesPressed++;
+            blueButtonPressed = true; 
+
+        }
+
+        public void YellowButtonPressed()
+        {
+            buttonsTimesPressed++;
+            yellowButtonPressed = true;
+            
+        }
+    
+        public void GreenButtonPressed()
+        {
+            buttonsTimesPressed++;
+            greenButtonPressed = true;
+            
+        } 
+    
+        public void RedButtonPressed()
+        {
+            buttonsTimesPressed++;
+            redButtonPressed = true;
+            
+        }
+    }
+}
