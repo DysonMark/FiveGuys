07472e1db05e1cbc52f092030f005e7e64bc167f
Dyson <dysonkui@gmail.com>
2024-07-07 23:38:01 +0400

    Reapply "Added bomb wires and functionate"
    This reverts commit 1d5103bc808db553d9ec89ae101b4ac7b81c80f0.

--- Assets/Scripts/Bomb/DefuseTheBomb.cs
+++ Assets/Scripts/Bomb/DefuseTheBomb.cs
@@ -6,42 +6,112 @@ namespace SAE.FiveGuys.Bomb
 {
     public class DefuseTheBomb : MonoBehaviour
     {
-        private int whichWire;
+        public List<int> whichWire = new List<int>();
 
+        private bool bluePass = false;
+
+        private bool redPass = false;
+
+        private bool yellowPass = false;
+
+        private bool greenPass = false;
         // Start is called before the first frame update
         void Start()
         {
-
+            
         }
 
         // Update is called once per frame
         void Update()
         {
-
+            DefuseOrNot();
         }
 
         public void BlueWire()
         {
             Debug.Log("Blue wire has been cut!");
-            whichWire = 1;
+            whichWire.Add(1);
+            if (whichWire == null)
+            {
+                Debug.Log("Bomb Exploded");
+            }
+            else if (whichWire[0] != 1)
+            {
+                Debug.Log("Bomb Exploded");
+            }
+            else
+            {
+                Debug.Log("Blue in right position!");
+                bluePass = true;
+            }
         }
 
         public void RedWire()
         {
             Debug.Log("Red wire has been cut!");
-            whichWire = 2;
+            whichWire.Add(2);
+            
+            if (whichWire.Count < 2)
+            {
+                Debug.Log("Bomb Exploded");
+            }
+            else
+            {
+                Debug.Log("Red in right position!");
+                redPass = true;
+            }
         }
 
         public void YellowWire()
         {
             Debug.Log("Yellow wire has been cut!");
-            whichWire = 3;
+            whichWire.Add(3);
+            if (whichWire.Count < 3)
+            {
+                Debug.Log("Bomb Exploded");
+            }
+            else if (whichWire[2] != 3)
+            {
+                Debug.Log("Bomb Exploded");
+            }
+            else
+            {
+                Debug.Log("Yellow in right position!");
+                yellowPass = true;
+            }
         }
 
         public void GreenWire()
         {
             Debug.Log("Green wire has been cut!");
-            whichWire = 4;
+            whichWire.Add(4);
+            if (whichWire.Count < 4)
+            {
+                Debug.Log("Bomb Exploded");
+            }
+            else if (whichWire[3] != 4)
+            {
+                Debug.Log("Bomb Exploded");
+            }
+            else
+            {
+                Debug.Log("Green in right position!");
+                greenPass = true;
+            }
+        }
+
+        private void DefuseOrNot()
+        {
+            for(int i = 0; i < whichWire.Count; i++)
+            {
+                Debug.Log("My list: " + whichWire[i]);
+            }
+
+            if (bluePass == true && redPass == true && yellowPass == true && greenPass == true)
+            {
+                Debug.Log("Bomb defused");
+            }
+           
         }
 
     }
