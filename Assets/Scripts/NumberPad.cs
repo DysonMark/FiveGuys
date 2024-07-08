4ce5663925c15b89960144142e5865b11d6ab6d8
Leon Colaco <leon.colaco@student.saedubai.com>
2024-07-08 13:16:59 +0400

    Numberpad scriptable object creation

---
+++ Assets/Scripts/NumberPad.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Keys")]
+public class NumberPad : ScriptableObject
+{
+    public int Key;
+    public int KeyValue;
+}
