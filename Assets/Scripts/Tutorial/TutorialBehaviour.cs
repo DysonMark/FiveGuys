66b6890c00d7ccb09a4aa5f70a6c6740262b6bf9
Dyson <dysonkui@gmail.com>
2024-07-16 23:06:44 +0400

    Finished tutorial for teleportation + added folder for Sounds

---
+++ Assets/Scripts/Tutorial/TutorialBehaviour.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using JW.FiveGuys.Teleportation;
+using UnityEngine;
+
+namespace SAE.FiveGuys.Tutorial
+{
+    public class TutorialBehaviour : MonoBehaviour
+    {
+        [SerializeField] private GameObject head;
+
+        public TeleportationController teleport;
+        // Start is called before the first frame update
+        void Start()
+        {
+            head = GameObject.Find("Head");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (teleport.isPlayerTeleporting == true)
+            {
+                Debug.Log("Player has teleported");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Tutorial/TutorialVoice.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using JW.FiveGuys.Teleportation;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace SAE.FiveGuys.Tutorial
+{
+    public class TutorialVoice : MonoBehaviour
+    {
+        [SerializeField] private AudioSource successfulTeleportation;
+        [SerializeField] private AudioSource stopStartSound;
+        public TeleportationController didHeTeleported;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            successfulTeleportation.Pause();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (didHeTeleported.isPlayerTeleporting == true)
+            {
+                successfulTeleportation.UnPause();
+                stopStartSound.Stop();
+            }
+            else if (didHeTeleported.isPlayerTeleporting == false)
+            {
+                successfulTeleportation.Pause();
+            }
+        }
+    }
+}
