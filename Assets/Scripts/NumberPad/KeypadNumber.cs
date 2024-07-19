73a5a5c95150ba5ed20f255ab712fe54f2d94022
Leon Colaco <leon.colaco@student.saedubai.com>
2024-07-19 20:45:59 +0400

    changes to keypadnumber script and numberpad script and adding comments

--- Assets/Scripts/NumberPad/KeypadNumber.cs
+++ Assets/Scripts/NumberPad/KeypadNumber.cs
@@ -7,52 +7,32 @@ using UnityEngine;
 
 public class KeypadNumber : MonoBehaviour
 {
-    private int Digit1;
-    private int Digit2;
-    private int Digit3;
-    private int Digit4;
+    [SerializeField] private TMP_Text displaycurrentSequence;
 
-    public TMP_Text Keydisplay1;
-    public TMP_Text Keydisplay2;
-    public TMP_Text Keydisplay3;
-    public TMP_Text Keydisplay4;
+    public NumberPad numberPad;
 
-    public List<int> correctSequence = new List<int>();
     public List<int> currentSequence = new List<int>();
-    void Start()
-    {
-        //Sequence Generator
-        Digit1 = Random.Range(0, 10);
-        Digit2 = Random.Range(0, 10);
-        Digit3 = Random.Range(0, 10);
-        Digit4 = Random.Range(0, 10);
-        //Adding the digits to the correct sequence
-        correctSequence[0] = Digit1;
-        correctSequence[1] = Digit2;
-        correctSequence[2] = Digit3;
-        correctSequence[3] = Digit4;
-        //Displaying of digits
-        Keydisplay1.text = Digit1.ToString();
-        Keydisplay2.text = Digit2.ToString();
-        Keydisplay3.text = Digit3.ToString();
-        Keydisplay4.text = Digit4.ToString();
-    }   
-    void Update()
-    {
-
-    }
 
     public void NumberPressed(int number)
     {
         currentSequence.Add(number);
-        if (currentSequence.Count > 3) 
+
+        //displaycurrentSequence.text = currentSequence.ToString();
+
+        if (currentSequence.Count == 4) 
         {
-            Enumerable.SequenceEqual(correctSequence, currentSequence);
+            bool check = Enumerable.SequenceEqual( numberPad.correctSequence, currentSequence);
+            if (check == true)
+            {
+                //Win condition for the game 
+            }
+            else
+            {
+                //if the sequence is wrong, it clears the numbers and the player can enter a new sequence 
+                currentSequence.Clear();
+            }
         }
-        else
-        {
-            currentSequence.Clear();
-        }
+        
         
     }
 }
--- Assets/Scripts/NumberPad/NumberPad.cs
+++ Assets/Scripts/NumberPad/NumberPad.cs
@@ -1,16 +1,53 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
+using Random = UnityEngine.Random;
 
 public class NumberPad : MonoBehaviour
 {
+    //Variables for each digit for the correct sequence 
+    private int Digit1;
+    private int Digit2;
+    private int Digit3;
+    private int Digit4;
+
+    //Display for the digits for the correct sequence
+    public TMP_Text Keydisplay1;
+    public TMP_Text Keydisplay2;
+    public TMP_Text Keydisplay3;
+    public TMP_Text Keydisplay4;
+
+    //
+    public List<int> correctSequence = new List<int>();
+
     [SerializeField] private UnityEvent onPressed;
     [SerializeField] private int number;
 
     private void OnTriggerEnter(Collider other)
     {
         onPressed.Invoke();
+    }
+    void Start()
+    { 
+        //Sequence Generator
+        Digit1 = Random.Range(0, 10);
+        Digit2 = Random.Range(0, 10);
+        Digit3 = Random.Range(0, 10);
+        Digit4 = Random.Range(0, 10);
+
+        //Adding the digits to the correct sequence
+        correctSequence.Add(Digit1);
+        correctSequence.Add(Digit2);
+        correctSequence.Add(Digit3);
+        correctSequence.Add(Digit4);
+
+        //Displaying of digits
+        Keydisplay1.text = Digit1.ToString();
+        Keydisplay2.text = Digit2.ToString();
+        Keydisplay3.text = Digit3.ToString();
+        Keydisplay4.text = Digit4.ToString();
     }
 }
