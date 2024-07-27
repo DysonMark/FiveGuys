43135d4d4c150c0b79cd351cb46fa98d653e2b05
JanWillem Goedvolk <caringsharing57@gmail.com>
2024-07-27 13:47:39 +0400

    Oculus Voice SDK Added
    Switched the flow voice controls over to the Oculus Voice SDK for more reliable voice detection

--- Assets/Scripts/Flow/FlowSpeechController.cs
+++ Assets/Scripts/Flow/FlowSpeechController.cs
@@ -5,51 +5,21 @@ using System;
 using System.Text;
 using UnityEngine.Windows.Speech;
 using UnityEngine.Events;
+using Oculus.Voice;
 
 namespace JW.FiveGuys.Flow
 {
     public class FlowSpeechController : MonoBehaviour
     {
-        [SerializeField] private string[] keywords;
-        [SerializeField] private FlowController controller;
-        private KeywordRecognizer recognizer;
+        [SerializeField] private KeyCode activateOn = KeyCode.Space;
+        [SerializeField] private AppVoiceExperience voiceControll;
 
         // Start is called before the first frame update
-        void Start()
+        void Update()
         {
-            controller = GetComponent<FlowController>();
-
-            recognizer = new KeywordRecognizer(keywords);
-            recognizer.OnPhraseRecognized += OnPhraseRecognized;
-            recognizer.Start();
-        }
-
-        private void OnPhraseRecognized(PhraseRecognizedEventArgs args)
-        {
-            Debug.Log(args.text);
-
-            switch (args.text)
+            if (Input.GetKeyUp(activateOn))
             {
-                case "North":
-                    controller.ButtonPressed(1);
-                    break;
-                case "East":
-                    controller.ButtonPressed(2);
-                    break;
-                case "South":
-                    controller.ButtonPressed(3);
-                    break;
-                case "West":
-                    controller.ButtonPressed(4);
-                    break;
-                case "Reset":
-                    controller.ButtonPressed(-1);
-                    break;
-                case "Draw":
-                    controller.ButtonPressed(-2);
-                    break;
-                default:
-                    break;
+                voiceControll.Activate();
             }
         }
     } 
