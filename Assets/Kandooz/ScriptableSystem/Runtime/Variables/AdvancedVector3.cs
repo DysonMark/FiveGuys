c4bc2a967ca07dbbe9ff3e1bcae9cee58a473f50
JanWillem Goedvolk <caringsharing57@gmail.com>
2024-07-27 15:03:27 +0400

    Flashlight Work
    Started working on making a better indication for where the moth will go to

---
+++ Assets/Kandooz/ScriptableSystem/Runtime/Variables/AdvancedVector3.cs
@@ -0,0 +1,10 @@
+
+using UnityEngine;
+
+namespace Kandooz.ScriptableSystem
+{
+	[CreateAssetMenu(menuName = "Kandooz/Scriptable System/Advanced Variables/Advanced Vector 3")]
+	public class AdvancedVector3 : AdvancedScriptableVariable<Vector3>
+	{
+	} 
+}
