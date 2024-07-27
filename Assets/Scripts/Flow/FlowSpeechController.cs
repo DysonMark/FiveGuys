7c38ff4369cbe65b632e658859d1849beae566d4
JanWillem Goedvolk <caringsharing57@gmail.com>
2024-07-27 14:12:20 +0400

    Added Controller Support
    Added the right primary (A button) to start the voice recognition

--- Assets/Scripts/Flow/FlowSpeechController.cs
+++ Assets/Scripts/Flow/FlowSpeechController.cs
@@ -6,6 +6,7 @@ using System.Text;
 using UnityEngine.Windows.Speech;
 using UnityEngine.Events;
 using Oculus.Voice;
+using System.ComponentModel;
 
 namespace JW.FiveGuys.Flow
 {
@@ -13,13 +14,24 @@ namespace JW.FiveGuys.Flow
     {
         [SerializeField] private KeyCode activateOn = KeyCode.Space;
         [SerializeField] private AppVoiceExperience voiceControll;
+        [SerializeField][ReadOnly(true)] private bool isActive = false;
+
 
         // Start is called before the first frame update
         void Update()
         {
-            if (Input.GetKeyUp(activateOn))
+            if (Input.GetAxis("XRI_Right_PrimaryButton") <= 1)
             {
-                voiceControll.Activate();
+                isActive = false;
+            }
+
+            if (Input.GetKeyUp(activateOn) || Input.GetAxis("XRI_Right_PrimaryButton") >= 1)
+            {
+                if (!isActive)
+                {
+                    voiceControll.Activate();
+                    isActive = true;
+                }
             }
         }
     } 
