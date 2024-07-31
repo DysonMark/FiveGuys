1d24a259074ef80afc35823b26f9a6a265e7f015
Leon Colaco <leon.colaco@student.saedubai.com>
2024-08-01 00:27:57 +0400

    changes from colliders to voice implementation

--- Assets/Scripts/NumberPad/KeypadNumber.cs
+++ Assets/Scripts/NumberPad/KeypadNumber.cs
@@ -20,7 +20,7 @@ public class KeypadNumber : MonoBehaviour
     public List<int> currentSequence = new List<int>();
 
     //Function for the number pressed by the player 
-    public void NumberPressed(int number)
+  /*public void NumberPressed(int number)
     {
         //sequence which will be printed when the player eneters the number
         sequence += number.ToString();
@@ -44,6 +44,58 @@ public class KeypadNumber : MonoBehaviour
                 displaycurrentSequence.text = ("Access Denied");
                 currentSequence.Clear();
             }
+        }
+    }*/
+    public void numberPressed(int index)
+    {
+        switch (index)
+        {
+            case 0:
+                currentSequence.Add(0);
+                break;
+            case 1:
+                currentSequence.Add(1);
+                break;
+            case 2:
+                currentSequence.Add(2);
+                break;
+            case 3:
+                currentSequence.Add(3);
+                break;
+            case 4:
+                currentSequence.Add(4);
+                break;
+            case 5:
+                currentSequence.Add(5);
+                break;
+            case 6:
+                currentSequence.Add(6);
+                break;
+            case 7:
+                currentSequence.Add(7);
+                break;
+            case 8:
+                currentSequence.Add(8);
+                break;
+            case 9:
+                currentSequence.Add(9);
+                break;
+            case 10:
+                bool check = Enumerable.SequenceEqual( numberPad.correctSequence, currentSequence);
+            if (check == true)
+            {
+                displaycurrentSequence.text = ("Access Granted");
+                //Win condition for the game 
+            }
+            else
+            {
+                //if the sequence is wrong, it clears the numbers and the player can enter a new sequence 
+                displaycurrentSequence.text = ("Access Denied");
+                currentSequence.Clear();
+            }
+                break;
+            
+
         }
     }
 }
--- Assets/Scripts/NumberPad/NumbersPressed.cs
+++ Assets/Scripts/NumberPad/NumbersPressed.cs
@@ -5,12 +5,5 @@ using UnityEngine.Events;
 
 public class NumbersPressed : MonoBehaviour
 {
-    [SerializeField] private UnityEvent onPressed;
-    [SerializeField] private int number;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("works");
-        onPressed.Invoke();
-    }
 }
