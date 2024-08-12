d4653384cfb88808419a277ba05eed0f20ec16db
JanWillem Goedvolk <caringsharing57@gmail.com>
2024-08-12 17:58:39 +0400

    Bomb Work
    Added UnityEvents that are invoked when the bomb is difused

--- Assets/Scripts/Bomb/DefuseTheBomb.cs
+++ Assets/Scripts/Bomb/DefuseTheBomb.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
 namespace SAE.FiveGuys.Bomb
@@ -20,7 +21,7 @@ namespace SAE.FiveGuys.Bomb
         public bool greenPass = false;
 
         public bool bombHasBeenDefused = false;
-
+        [SerializeField] private UnityEvent onSolved;
         public bool bombHasExploded;
         public BombCountdown timeIsUp;
         
@@ -97,6 +98,7 @@ namespace SAE.FiveGuys.Bomb
             if (bluePass == true && redPass == true && yellowPass == true && greenPass == true)
             {
                 bombHasBeenDefused = true;
+                if (onSolved != null) { onSolved?.Invoke(); }
             }
 
             if (timeIsUp.holdCounting <= 0)
