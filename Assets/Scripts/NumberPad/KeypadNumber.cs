81830e1d6fc1299c23709ab4e9ec4041da4c1d43
JanWillem Goedvolk <caringsharing57@gmail.com>
2024-08-12 17:19:15 +0400

    Attempted build

--- Assets/Scripts/NumberPad/KeypadNumber.cs
+++ Assets/Scripts/NumberPad/KeypadNumber.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using TMPro;
-using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
 using UnityEngine.Events;
 
